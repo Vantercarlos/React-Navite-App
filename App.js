021d87108f108832b5ef7840ec181eb773649496
vantercarlos <vantercarlosti@gmail.com>
2021-07-01 11:18:58 -0400

    Adicionado as bibliotecas expo-Location Expo-Permissions

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState,useEffect,useRef} from 'react';
+import {Text, View} from 'react-native';
 import {css} from './assets/css/css';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+
+  const [origin,setOrigin]=useState(null);
+  const [destination,setDestination]=useState(null);
+
+  useEffect(()=>{
+    (async function(){
+      const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status === 'granted') {
+        let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+        setOrigin({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0922,
+        })
+      } else {
+        throw new Error('Location permission not granted');
+      }
+    })();
+  },[]);
+
   return (
     <View style={css.container}>
       <MapView 
         style={css.map} 
-        initialRegion={{
-          latitude: 2.82065,
-          longitude: -60.67215,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={origin}
         showsUserLocation={true}
         loadingEnabled={true}
       >
